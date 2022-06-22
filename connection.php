f0426c42199601d8d26cadd58580db2346cd069e
Rian Wahyu <rianwahyu26@gmail.com>
2022-06-22 15:46:47 +0700

    custom ftp

--- connection.php
+++ connection.php
@@ -1,6 +1,9 @@
 <?php
 
-$dbc = mysqli_connect('localhost', 'rige4492_root', '8umx9E5#RyNepdda', 'rige4492_istia') 
+date_default_timezone_set('Asia/Jakarta');
+error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
+
+$dbc = mysqli_connect('localhost', 'u1687666_root', '8umx9E5#RyNepdda', 'u1687666_istia_apps') 
 OR die('Cannot Conect to database because '.mysqli_connect_error());
 
 ?>
